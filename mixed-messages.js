c85241cba3e6fe3505709abd7451a9af3adb1017
Sergiu Apostul <sergiu.apostul@gmail.com>
2021-10-07 07:31:17 +0300

    Sports arrays created

--- mixed-messages.js
+++ mixed-messages.js
@@ -6,3 +6,9 @@ truly random, the message that it outputs should be made up of at least three
 different pieces of data. Take what you know of JavaScript syntax so far to 
 build the program and customize it to your liking. */
 
+
+const olympicGames = ['3x3 Basketball', 'Archery', 'Artistic Gymnastics', 'Artistic Swimming', 'Athletics', 'Badminton', 'Baseball Softball', 'Basketball', 'Beach Volleyball', 'BMX Freestyle', 'BMX Racing', 'Boxing', 'Canoe/Kayak Slalom', 'Diving', 'Equestrian', 'Fencing', 'Fotball', 'Golf', 'Handball', 'Hockey', 'Judo', 'Karate', 'Marathon Swimming', 'Modern Penthatlon', 'Mountain Bike', 'Rhytmic Gymnastics', 'Road Cycling', 'Rowing', 'Rugby', 'Shooting', 'Skateboarding', 'Sport Climbing', 'Surfing', 'Swimming', 'Table Tennis', 'Taekwondo', 'Tennis', 'Track Cycling', 'Trampoline', 'Triathlon', 'Volleyball', 'Water Polo', 'Weightlifting', 'Wrestling']; //46
+
+const olympicWinterGames = ['Alpine Skiing', 'Biathlon', 'Bobsleigh', 'Cross-country', 'Curling', 'Figure Skating', 'Freestyle Skiing', 'Ice Hockey', 'Luge', 'Nordic Combined', 'Short Track Speed Skating', 'Skeleton', 'Ski Jumping', 'Snowboard', 'Speed Skating']; //15 
+
+const eSports = ['Dota 2', 'Counter-Strike: Global Offensive', 'Fortnite', 'League of Legends', 'StarCraft II', "PlayerUnknown's Battlegrounds (PUBG)", 'Overwatch', 'Hearthstone']; //8
